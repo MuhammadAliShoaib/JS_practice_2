b2da519f2ebf9e60b84bd8bb28a04dfcf4ab5408
MuhammadAliShoaib <malishoaib.2001@gmail.com>
2022-11-05 07:14:03 +0500

    Project Started

---
+++ script.js
@@ -0,0 +1,103 @@
+// var abc = "Muhammad Ali ";
+// console.log(abc);
+
+// var ab = prompt("Enter number : ");
+// console.log(ab);
+
+// var firstName = prompt("Enter your first name : ");
+// var lastName = prompt("Enter your Last name : ");
+// //  default value written after the msg followed by coma 
+// var city = prompt("Enter your city : ","Karachi");
+
+// console.log(firstName+" "+lastName)
+// console.log(city)
+
+// var num1 = prompt("Enter first number : ");
+// var num2 = prompt("Enter second number : ");
+
+// var result = parseInt(num1) + parseInt(num2);
+// console.log("Additon : ",result);
+
+//  result = parseInt(num1) - parseInt(num2);
+// console.log("Subraction : ",result);
+
+//  result = parseInt(num1) * parseInt(num2);
+// console.log("Multiplication : ",result);
+
+// result = parseInt(num1) / parseInt(num2);
+// console.log("Division : ",result);
+
+// var officer = false;
+// var assistant = true;
+// if(officer){
+//     console.log("Document submitted")
+// }else if(assistant){
+//     console.log("Document submitted by assistant")
+// }else{
+//     console.log("Document not submitted")
+// }
+
+// var age = 18;
+// if(age<18){
+//     alert("Driving not allowed under age")
+// }else if(age>60){
+//     alert("Driving not allowed over aged ")
+// }else{
+//     alert("Driving allowed")
+// }
+
+
+// +prompt used for parsing
+// var marks1 = +prompt("Enter marks of 1st subject : ");
+// var marks2 = +prompt("Enter marks of 2nd subject : ");
+// var marks3 = +prompt("Enter marks of 3rd subject : ");
+// var marks4 = +prompt("Enter marks of 4th subject : ");
+// var marks5 = +prompt("Enter marks of 5th subject : ");
+
+// var sum = marks1+marks2+marks3+marks4+marks5;
+// var percentage = (sum/500)*100;
+// console.log(percentage);
+
+// if(percentage>=80){
+//     console.log("A grade")
+// }else if(percentage>=70){
+//     console.log("B Grade");
+// }else if(percentage>=60){
+//     console.log("C Grade");
+// }else if(percentage>=50){
+//     console.log("D Grade");
+// }else{
+//     console.log("Failed")
+// }
+
+// var a = 10; 
+// var b = 10;
+// var c = 5;
+// // three equals check value and data type both
+// if(a===b){
+//     console.log("A is equal to B")
+// }
+
+// if(a>c && a<b){
+//     console.log("A is greater than C and A is less than B")
+// }
+
+// var age = 18;
+// var studentCard = true;
+
+// if(age==18){
+//     console.log("Allow");
+// }else{
+//     if(studentCard==true){
+//         console.log("Allow on student card")
+//     }else{
+//         console.log("Not Allow");
+//     }
+// }
+
+// if(age==18 || studentCard==true){
+//     console.log("Allow");
+// }else{
+//     console.log("Not Allow"); 
+// }
+
